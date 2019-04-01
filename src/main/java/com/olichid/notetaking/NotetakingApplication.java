71c98cd1c890df0ce07732bcaffe23391db4ad44
olichid <zhengchengc@student.unimelb.edu.au>
2019-04-01 22:38:41 +1100

    initial the project

---
+++ src/main/java/com/olichid/notetaking/NotetakingApplication.java
@@ -0,0 +1,13 @@
+package com.olichid.notetaking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotetakingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotetakingApplication.class, args);
+	}
+
+}
